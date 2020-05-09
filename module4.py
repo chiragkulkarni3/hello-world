c660519e421abea896d490b148c0ab1a2444a8c1
chiragkulkarni3 <chiragkulkarni3@gmail.com>
2020-05-09 12:43:53 -0400

    Adding exercise files

---
+++ module4.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+
+"""
+
+#Exercise 1
+
+from datetime import datetime
+
+cur = datetime.now()
+
+curTime = cur.strftime("%H:%M:%S")
+print(curTime)
+
+#Exercise 2
+
+start = datetime.now()
+end= datetime.now()
+
+diff = end-start
+
+print(diff)
+
+#Exercise 3
+
+#Exercise 4
+
+#Exercise 5
+
+
+
+
---
+++ module4ex.R
@@ -0,0 +1,11 @@
+
+#Exercise 1
+print(Sys.time())
+
+#Exercise 2
+
+#Exercise 3
+
+#Exercise 4
+
+#Exercise 5
